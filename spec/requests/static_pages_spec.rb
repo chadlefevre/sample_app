095ae899273203cff10d07054cd218b32f203662
Chad Lefevre <chad.lefevre@beergnome.net>
2014-05-24 22:43:08 -0400

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,31 +2,37 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:pageTitle) { "Ruby on Rails Tutorial Sample App" }
+  let(:dividerTitle) { " | " }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+    	visit '/static_pages/home'
+    	expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title(pageTitle)
+    end
+
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	expect(page).not_to have_title(dividerTitle + "Home")
     end
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
+    	visit '/static_pages/help'
+    	expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
     	visit '/static_pages/help'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    	expect(page).to have_title(pageTitle + dividerTitle + "Help")
     end
   end
 
@@ -37,9 +43,22 @@ describe "StaticPages" do
   		expect(page).to have_content('About Us')
   	end
 
-    it "should have the title 'About'" do
+    it "should have the title 'About Us'" do
     	visit '/static_pages/about'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    	expect(page).to have_title(pageTitle + dividerTitle + "About Us")
+    end
+  end
+
+  describe "Contact Page" do
+
+  	it "should have the content 'Contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('contact')
+  	end
+
+    it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title(pageTitle + dividerTitle + "Contact")
     end
   end
 end
